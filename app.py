ff1e20d9ccf31fce446a63ffb0527ee8cdcd69e9
yashin shekh <nyasin585@gmail.com>
2020-10-14 11:31:53 +0545

    asdf

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
+from browsermobproxy import Server
+import psutil
+import time
 
-if __name__ == '__main__':
-    print("test")
+for proc in psutil.process_iter():
+    # check whether the process name matches
+    if proc.name() == "browsermob-proxy":
+        proc.kill()
 
-    with open("test.txt","w") as f:
-        f.write("anything")
+dict = {'port': 8090}
+server = Server(path="./BrowserMobProxy/bin/browsermob-proxy", options=dict)
+server.start()
+time.sleep(1)
+proxy = server.create_proxy()
+time.sleep(1)
+from selenium import webdriver
+profile = webdriver.FirefoxProfile()
+selenium_proxy = proxy.selenium_proxy()
+profile.set_proxy(selenium_proxy)
+driver = webdriver.Firefox(firefox_profile=profile)
 
-    # asdf
+
+proxy.new_har("google")
+driver.get("http://www.google.co.uk")
+print (proxy.har) # returns a HAR JSON blob
+
+server.stop()
+driver.quit()
